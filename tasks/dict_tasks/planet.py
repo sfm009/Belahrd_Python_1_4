0b55501af4e487e94eb808cfd357c2833199cd21
unknown <sfm009@gmail.com>
2022-07-27 22:47:09 +0300

    dict planet

--- tasks/dict_tasks/planet.py
+++ tasks/dict_tasks/planet.py
@@ -19,10 +19,12 @@
 --------------------------------------------------------------------------------
 get_planet_name(3) -> 'Земля'
 """
-
+planets = {1: 'Меркурий', 2: 'Венера', 3: 'Земля', 4: 'Марс',
+           5: 'Юпитер', 6: 'Сатурн', 7: 'Уран', 8: 'Нептун',
+           20: None}
 
 def get_planet_name(planet_num: int) -> str:
-    result = None
+    result = planets[planet_num]
     return result
 
 
